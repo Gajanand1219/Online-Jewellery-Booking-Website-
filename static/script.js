0350706cff3fe97445b56e6fd5e1876d65c34ac1
Gajanan Deshmukh <159412495+Gajanand1219@users.noreply.github.com>
2025-01-09 19:31:50 +0530

    Update script.js
    Signed-off-by: Gajanan Deshmukh  <159412495+Gajanand1219@users.noreply.github.com>

--- static/script.js
+++ static/script.js
@@ -19,7 +19,7 @@ function openNav() {
 function myMap() {
     var mapProp = {
         center: new google.maps.LatLng(40.712775, -74.005973),
-        zoom: 18,
+        zoom: 17,
     };
     var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
 }
